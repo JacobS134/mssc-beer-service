73154932f23b9affe26b71d468cec479023aeef8
Jacob Smith <jacob.smith@saggezza.com>
2021-03-01 18:18:02 +0000

    Initial commit

---
+++ src/test/java/jacob/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jacob.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
