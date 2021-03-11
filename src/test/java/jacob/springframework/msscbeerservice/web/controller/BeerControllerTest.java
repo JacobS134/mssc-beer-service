0daba62af8aa217e8f2a96936e1a2c6c593371c0
Jacob Smith <jacob.smith@saggezza.com>
2021-03-11 11:52:06 +0000

    Validation

--- src/test/java/jacob/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/jacob/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,6 +1,5 @@
 package jacob.springframework.msscbeerservice.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jacob.springframework.msscbeerservice.web.modal.BeerDto;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.UUID;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,14 +23,12 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
     }
 
     @Test
     void saveNewBeer() throws Exception {
-
         BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
@@ -44,7 +40,6 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-
         BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
